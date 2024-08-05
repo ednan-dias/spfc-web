9282cb498b7d9c9e95718144e43508baefb6b3aa
belahome <ti@belahome.com.br>
2024-08-05 16:25:10 -0300

    chore: add lint-staged, prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      pluginReactHooks,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  { settings: { react: { version: 'detect' } } },
+];
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,3 +1,3 @@
 export function Dashboard() {
-  return "Dashboard";
+  return 'Dashboard';
 }
